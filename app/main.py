b8528e4bb06321a430ee4463cab51faa9f187989
Emmanuel LASTRA <emmanuellastra@gmail.com>
2025-05-10 18:49:08 +0200

    fix api settings mount

--- app/main.py
+++ app/main.py
@@ -1,20 +1,18 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
-from app.routes import pages, capture
+from app.routes import pages, capture, settings  # Add settings import here
 
 
 app = FastAPI()
 
-#Monter le répertoire statique pour servir les fichiers CSS, JS, etc.
+# Monter le répertoire statique pour servir les fichiers CSS, JS, etc.
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-#Routeur vers les pages
+# Routeur vers les pages
 app.include_router(pages.router, tags=["pages"])
 
-#Routeur vers l'API de capture
+# Routeur vers l'API de capture
 app.include_router(capture.router, prefix="/api/capture", tags=["capture"])
-#Routeur vers l'API de paramètres
-app.include_router(capture.router, prefix="/api/settings", tags=["settings"])
-
-
+# Routeur vers l'API de paramètres
+app.include_router(settings.router, prefix="/api/settings", tags=["settings"]) 
